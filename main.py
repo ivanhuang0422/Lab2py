880d9fa763987f563169d5d52eba6b04175a7628
YuHsiang Huang <ykh5222@psu.edu>
2020-09-11 02:56:01 +0000

    First try

---
+++ main.py
@@ -0,0 +1,30 @@
+# Author: Yu-Hsiang, Huang ykh5222@psu.edu
+# Collabrator: Wenjun Ju wkj5070@psu.ed
+# Collabrator: Aracind Hariprased azh5899@psu.edu
+# Collabrator: Abishek Venkataraman apv5314@psu.edu
+# Section: 4
+# Breakout: 11
+def getLetterGrade(grade):
+  return "A"
+def run():
+  grade=float(input("Enter Your CMPSC 131 grade: "))
+  if grade>=93.0:
+    print(f"Your letter grade for CMPSC 131 is A.")
+  elif grade>=90.0:
+    print(f"Your letter grade for CMPSC 131 is A.")
+  elif grade>=87.0:
+    print(f"Your letter grade for CMPSC 131 is B+.")
+  elif grade>=83.0:
+    print(f"Your letter grade for CMPSC 131 is B.")
+  elif grade>=80.0:
+    print(f"Your letter grade for CMPSC 131 is B-.")
+  elif grade>=77.0:
+    print(f"Your letter grade for CMPSC 131 is C+.")
+  elif grade>=70.0:
+    print(f"Your letter grade for CMPSC 131 is C.")
+  elif grade>=60.0:
+    print(f"Your letter grade for CMPSC 131 is D.")
+  else:
+    print(f"Your letter grade for CMPSC 131 is F.")
+if __name__=="__main__":
+  run()
