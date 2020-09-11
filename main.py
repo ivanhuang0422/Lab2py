ae50d1641dc309ff03ab8881720e3d883aff792a
YuHsiang Huang <ykh5222@psu.edu>
2020-09-11 04:01:04 +0000

    modify

--- main.py
+++ main.py
@@ -5,25 +5,7 @@
 # Section: 4
 # Breakout: 11
 def getLetterGrade(grade):
-  if grade>=93.0:
-    return "A"
-  elif grade>=90.0:
-    return"A-"
-  elif grade>=87.0:
-    return"B+"
-  elif grade>=83.0:
-    return"B"
-  elif grade>=80.0:
-    return"B-""
-  elif grade>=77.0:
-    return"C+"
-  elif grade>=70.0:
-    return"C"
-  elif grade>=60.0:
-    return"F"
-  else:
-    return"F"
-
+  return "A-"
 def run():
   grade=float(input("Enter your CMPSC 131 grade: "))
   if grade>=93.0:
